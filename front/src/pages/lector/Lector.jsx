1380f35f3f82a58f5c154a8f3334fb5069314fcf
Bryan406 <rquisper406@gmail.com>
2025-08-08 11:44:59 -0500

    Configuration in frontend the lector see for evidences the process by event

--- front/src/pages/lector/Lector.jsx
+++ front/src/pages/lector/Lector.jsx
@@ -91,7 +91,7 @@ export default function Lector() {
   return (
     <>
       <div className="max-w-7xl mx-auto p-6">
-        <h1 className="text-3xl font-bold text-center mb-8 text-gray-800">
+        <h1 className="text-3xl font-bold text-center mb-8 text-white">
           Procesos Públicos
         </h1>
         {/* Filtros */}
