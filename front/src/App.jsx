5e5006e8cc821a1e614a3060ccdbe3c91fcde9e9
Bryan406 <rquisper406@gmail.com>
2025-08-08 10:27:19 -0500

    Configuration forntend

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter, Outlet, Route, Router, Routes } from 'react-router-dom';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 import LoginLayout from './layouts/loginLayout.jsx';
 import './App.css';
 import LayoutLawyer from './layouts/layoutLawyer.jsx';
@@ -9,7 +9,6 @@ import EvidenciasLectura from './pages/lector/EvidenciasLectura.jsx';
 import Lector from './pages/lector/Lector.jsx';
 import ObservacionesLectura from './pages/lector/ObservacionesLectura.jsx';
 import ProcesoResumen from './pages/lector/ProcesoResumen.jsx';
-import './App.css';
 import ReaderLayout from './pages/lector/readerLayout.jsx';
 import CaseDashboard from './lawyer/CaseDashboard.jsx';
 import CaseInfo from './lawyer/CaseInfo.jsx';
@@ -21,44 +20,45 @@ import ReminderList from './lawyer/ReminderList.jsx';
 import AuditList from './lawyer/AuditList.jsx';
 import RelatedCases from './lawyer/RelatedCases.jsx';
 import CaseCreate from './lawyer/CaseCreate.jsx';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginLayout />}></Route>
+        <Route path="/login" element={<LoginLayout />} />
         <Route path="/lawyer" element={<LayoutLawyer />}>
-          <Route path="/dashboard" element={<CaseDashboard />} />
-          <Route path="/account" element={<AccountData />} />
-          <Route path="/case-info/:id" element={<CaseInfo />} />
-          <Route path="/event-dashboard/:caseId" element={<EventDashboard />} />
-          <Route path="/create-case" element={<CaseCreate />} />
-          <Route path="/evidences/:eventId" element={<EvidenceDashboard />} />
+          <Route path="dashboard" element={<CaseDashboard />} />
+          <Route path="account" element={<AccountData />} />
+          <Route path="case-info/:id" element={<CaseInfo />} />
+          <Route path="event-dashboard/:caseId" element={<EventDashboard />} />
+          <Route path="create-case" element={<CaseCreate />} />
+          <Route path="evidences/:eventId" element={<EvidenceDashboard />} />
           <Route
-            path="/observations/:eventId"
+            path="observations/:eventId"
             element={<ObservationDashboard />}
           />
           <Route
-            path="/pending-calendar/:caseId"
+            path="pending-calendar/:caseId"
             element={<PendingCalendar />}
           />
-          <Route path="/reminders" element={<ReminderList />} />
-          <Route path="/audit-list" element={<AuditList />} />
-          <Route path="/related-cases/:caseId" element={<RelatedCases />} />
+          <Route path="reminders" element={<ReminderList />} />
+          <Route path="audit-list" element={<AuditList />} />
+          <Route path="related-cases/:caseId" element={<RelatedCases />} />
         </Route>
-        <Route path="/unauthorized" element={<LayoutUnauthorized />}></Route>
+        <Route path="/unauthorized" element={<LayoutUnauthorized />} />
         <Route path="/" element={<ReaderLayout />}>
-          <Route index={true} element={<Lector />}></Route>
-          <Route path="/lector" element={<Lector />}></Route>
+          <Route index element={<Lector />} />
+          <Route path="lector" element={<Lector />} />
           <Route
-            path="/lector/proceso/:processId"
+            path="lector/proceso/:processId"
             element={<ProcesoResumen />}
           />
           <Route
-            path="/procesos/:processId/observaciones"
+            path="procesos/:processId/observaciones"
             element={<ObservacionesLectura />}
           />
           <Route
-            path="/procesos/:processId/evidencias"
+            path="procesos/:processId/evidencias"
             element={<EvidenciasLectura />}
           />
           <Route path="*" element={<div>404 Not Found</div>} />
